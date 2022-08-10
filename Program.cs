9265129b216fb974a0381d968c5466873a60f93c
mathbueeno <mathbueeno@gmail.com>
2022-08-10 15:40:10 -0300

    Mudança subtração

--- Program.cs
+++ Program.cs
@@ -6,17 +6,43 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Soma();
+        }
+
+        static void Soma()
+
+        {
             Console.Clear();
+
             Console.WriteLine("Primeiro Valor: ");
             float v1 = float.Parse(Console.ReadLine());
 
             Console.WriteLine("Segundo Valor: ");
             float v2 = float.Parse(Console.ReadLine());
 
+            Console.WriteLine("");
 
             float resultado = v1 + v2;
-            Console.WriteLine("O resultado da soma é: " + resultado);
+            Console.WriteLine("O resultado da soma é: {resultado}");
+
+        }
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro Valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine("O resultado da subtração é: {resultado}");
+            Console.ReadKey();
 
         }
     }
 }
+
